311d1e2138c1a69a8f1c79434842fe4348aa7c25
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-29 20:36:35 -0300

    imports

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ from flask import Flask, renderTemplate,url_for, request
 import pandas as pd
 import numpy as np
 from nltk.stem.porter import PorterStemmer 
+import re 
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+prom sklearn.linear_model import logisticRegression
+
+def remove_pattern(input_text.pattern):
