3984adf383b87cfd01925462844b03cd621d66b9
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-06 20:12:23 -0300

    flaskWebApi

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ def remove_pattern(input_txt, pattern):
     return input_txt
 
 def count_punct(text):
-    count = sum(1 for char in text if char in string.punctuation)
+    count = sum([1 for char in text if char in string.punctuation])
+    return round(count/len(text) - text.count(" "),3)*100 
+
+app = 
+flask(__name__)
+data = pd.read_csv("sentiment.tsv".sep ='\t')
     
